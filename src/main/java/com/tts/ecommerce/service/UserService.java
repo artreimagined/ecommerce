d5bceee5fdb2d48a6b7a957755569e7f8ee6a422
artreimagined <teri.t.davis@gmail.com>
2021-02-25 17:27:44 -0500

    "testing the UserService"

--- src/main/java/com/tts/ecommerce/service/UserService.java
+++ src/main/java/com/tts/ecommerce/service/UserService.java
@@ -24,7 +24,7 @@ public class UserService implements UserDetailsService {
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
-
+//testing testing
     public void saveNew(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
